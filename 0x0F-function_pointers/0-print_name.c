924b1a77f4fb4f24555aa2cedface7a8b3b0ed15
emma <esaina34@gmail.com>
2023-07-19 09:43:04 -0700

    files amended

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,14 @@
 #include "function_pointers.h"
-#include <stdio.h>
+
 /**
  * print_name - function that prints a name
- * @name: name to print
- * @f: pointer to a function to use to print name
  *
- * Return: none
+ * @name: THis is the input string or the input name
+ * @f: This is the input function to print the name
+ *
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
+	if (f != 0 && name != 0)
 		f(name);
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
 /**
- * int_index - searches for an integer
- * @array: array to be searched
- * @size: size of the array
- * @cmp: function to compare
+ * int_index - function that searches for an integer.
  *
- * Return: return integer
+ * @size: This is the length of the array
+ * @array: This is input array
+ * @cmp: Is a pointer to the function to be used to compare values
+ *
+ * Return: int_index returns the index of the first element for which the cmp
+ *         function does not return 0
+ *         If no element matches, return -1
+ *         If size <= 0, return -1
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, r;
+	int index;
 
-	if (array && cmp && size > 0)
+	if (array == NULL)
+		return (-1);
+	if (size <= 0)
+		return (-1);
+	if (cmp == NULL)
+		return (-1);
+	for (index = 0; a < size; index++)
 	{
-		for (i = 0; i < size; i++)
+		if ((cmp(array[index])) == 1)
 		{
-			if cmp(array[i]);
-
-				break;
+			return (index);
 		}
-		if (i < size)
-			return (i);
 	}
 	return (-1);
 }
