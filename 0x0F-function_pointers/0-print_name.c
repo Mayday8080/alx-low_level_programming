de763008b80fbbdbfed111734a44a033a77e8920
emma <esaina34@gmail.com>
2023-07-19 09:44:58 -0700

    files amend

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,16 @@
 #include "function_pointers.h"
-
 /**
- * print_name - function that prints a name
- *
- * @name: THis is the input string or the input name
- * @f: This is the input function to print the name
- *
- */
+ * print_name- this functions prints a name
+(* a blank line
+ *@name: string to print.
+ *@f: this a function pointer, that recieves a string.
+* Description: this function prints a name)?
+(* section header: the header of this function is function_pointers.h)*
+* Return: this is a void function no return
+*/
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != 0 && name != 0)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+	(*f)(name);
 }
